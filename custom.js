d17e856bb26ecdf0f6f43eff7742949bf6b79d3e
Metsu <13114199adarsh@gmail.com>
2022-11-06 08:14:16 +0000

    done

--- custom.js
+++ custom.js
@@ -1,4 +1,6 @@
 var logo = ("F a r m e r &nbsp; D e l i g h t ").split(" ");
+var slogan = (" F a s t&nbsp; <p></p> F o o d,&nbsp; <p></p> S l o w&nbsp; <p></p> p r i c e s,&nbsp; <p></p> F a s t&nbsp; <p></p> s e r v i c e ...").split(" ");
+var intro = ("G a d w a l l&nbsp; <p></p> I n s p i r e d&nbsp; <p></p> T a s t e ...").split(" ");
 var trans_time = .4;
 $(window).on("load", function () {
     setTimeout(function () {
@@ -17,6 +19,26 @@ $(window).on("scroll", function () {
     }
 });
 $(document).ready(function () {
+    function animateText($class, text, display) {
+        if(display == "block") {
+            text.forEach((e) => {
+                var text = `<span style='transition-delay: ${trans_time.toFixed(2)}s;'>${e}</span><br />`;
+                var $span = $(text);
+                $class.append($span);
+                trans_time += 0.05;
+            });
+        }
+        else if(display == "inline") {
+            text.forEach((e) => {
+                var text = `<span style='transition-delay: ${trans_time.toFixed(2)}s;'>${e}</span>`;
+                if(e.endsWith("&nbsp;"))
+                    text += "<p></p>"
+                var $span = $(text);
+                $class.append($span);
+                trans_time += 0.05;
+            });
+        }
+    }
     setTimeout(function (){
         $(".wrapper").removeClass("hidden")
     }, 1_000);
@@ -27,11 +49,7 @@ $(document).ready(function () {
         $("body").toggleClass("isMenu");
         $(".fa-times").toggleClass("fa-bars");
     });
-    var $textLogo = $(".side-text-logo");
-    logo.forEach((e) => {
-        var text = `<span style='transition-delay: ${trans_time.toFixed(2)}s;'>${e}</span><br />`;
-        var $p = $(text);
-        $textLogo.append($p);
-        trans_time += 0.1;
-    });
+    animateText($(".title-header .slogan"), slogan, "inline");
+    animateText($(".title-header .intro"), intro, "inline");
+    animateText($(".side-text-logo"), logo, "block");
 });
